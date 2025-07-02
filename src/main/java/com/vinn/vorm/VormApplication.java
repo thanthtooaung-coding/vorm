041cf42f528a070828d01f4b6e6ffeb9d0235744
thanthtooaung <thanthtoo1285@gmail.com>
2025-07-02 10:51:48 +0700

    initial commit

---
+++ src/main/java/com/vinn/vorm/VormApplication.java
@@ -0,0 +1,13 @@
+package com.vinn.vorm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VormApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vinn/vorm/VormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinn.vorm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
